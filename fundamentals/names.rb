604d83cbc1602a48981bf324637a9cda7250dc43
richnamk <richnamk@gmail.com>
2017-11-28 09:55:15 -0600

    names

---
+++ fundamentals/names.rb
@@ -0,0 +1,22 @@
+=begin
+Create a ruby method that goes over each name in the names array and prints something like below.
+You have 5 names in the 'names' array
+The name is 'Michael Choi'
+The name is 'John Doe'
+The name is 'Jane Doe'
+The name is 'James Smith'
+The name is 'Jennifer Smith'
+=end
+
+a = {first_name: "Michael", last_name: "Choi"}
+b = {first_name: "John", last_name: "Doe"}
+c = {first_name: "Jane", last_name: "Doe"}
+d = {first_name: "James", last_name: "Smith"}
+e = {first_name: "Jennifer", last_name: "Smith"}
+names = [a, b, c, d, e]
+
+puts "you have #{names.length} names in the 'names' array"
+names.each do |name|
+    puts "the name is #{name[:first_name]} #{name[:last_name]}"
+end
+
