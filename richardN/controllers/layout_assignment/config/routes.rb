ee86ef1fda193cdf0180c3eac0183cf0259c1cfd
richnamk <richnamk@gmail.com>
2017-12-06 15:06:48 -0600

    mommaSaidLayYouOut

--- richardN/controllers/layout_assignment/config/routes.rb
+++ richardN/controllers/layout_assignment/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :create]
+  resources :posts, only: [:index, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
