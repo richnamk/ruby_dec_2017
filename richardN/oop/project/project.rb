1c3df053ec9039c94394c5fdd391967e5bb1f605
richnamk <richnamk@gmail.com>
2017-12-06 20:27:14 -0600

    ameritrade

--- richardN/oop/project/project.rb
+++ richardN/oop/project/project.rb
@@ -1,16 +1,28 @@
 class Project
-  attr_accessor :name, :description
-
-  def initialize(name, description)
+  attr_accessor :name, :description, :owner
+  attr_reader :tasks
+  def initialize name, description, owner
     @name = name
     @description = description
+    @owner = owner
+    @tasks = []
   end
 
   def elevator_pitch
     "#{@name}, #{@description}"
   end
+
+  def print_tasks
+    @tasks.each { |elem| puts elem }
+  end
+
+  def add_tasks task
+    @tasks << task
+  end
 end
   
   project1 = Project.new("Project 1", "Description 1")
+  project2 = Project.new("Project 2", "Description 2")
+  
   puts project1.name
   puts project1.elevator_pitch
